c26f8d09ef93acc5636d5c827839bdde548fb825
ksu <ksundelife@yandex.ru>
2021-12-14 15:41:03 +0300

    file system

--- index.js
+++ index.js
@@ -1,106 +1,70 @@
-const colors = require("colors/safe");
-const EventEmitter = require('events');
-const emitter = new EventEmitter();
+///////МОЙ ВАРИАНТ РЕШЕНИЯ (ПРОВЕРЕН НА БОЛЬШОМ ФАЙЛЕ весом 2Гб)
 
-let [hour, day, month, year] = process.argv.slice(2);
+const fs = require('fs');
 
-let timerId = null;
+const readStream = fs.createReadStream('./access.log', 'utf8');
+const writeStream1 = fs.createWriteStream('./89.123.1.41_requests.log');
+const writeStream2 = fs.createWriteStream('./34.48.240.111_requests.log');
 
-const errorNaN = () => {
-	clearInterval(timerId);
-	console.log(colors.red('Упс! Не переживайте! Введите числом через пробел входные параметры в формате - <час> <день> <месяц> <год>. Параметры <день> <месяц> <год> должны быть больше 0. Пример: 20 1 1 2022'));
-}
+const {
+	Transform
+} = require('stream');
 
-const errorLessCurrentDate = () => {
-	clearInterval(timerId);
-	console.log(colors.red('Упс! Дата дедлайна не может быть меньше текущей. Не переживайте! Введите еще раз числом через пробел входные параметры в формате - <час> <день> <месяц> <год>. Параметры <день> <месяц> <год> должны быть больше 0. Пример: 20 1 1 2022'));
-}
+const transformStream1 = new Transform({
+	transform(chunk, encoding, callback) {
+		const transformedChunk = chunk.toString();
 
-const finish = () => {
-	clearInterval(timerId);
-	console.log(colors.blue('Ура, таймер завершен!'));
-}
+		let reg = new RegExp(`^.*(89.123.1.41).*$`, 'gm');
+		let result = transformedChunk.match(reg);
 
-function checkingInputParameters(hour, day, month, year, deadline) {
-	if (((day && month && year) !== 0) && ((hour && day && month && year) instanceof Number || typeof (hour && day && month && year) === 'number') && !isNaN(hour && day && month && year)) {
-		if ((deadline - new Date()) <= 0) {
-			return emitter.emit('errorLessCurrentDate');
-		} else if ((deadline - new Date()) === 0) {
-			return emitter.emit('finish');
-		} else {
-			return emitter.emit('timer', () => timer());
-		}
-	} else {
-		return emitter.emit('errorNaN');
+		result.forEach(element => {
+			this.push(element + "\n");
+		});
+		callback();
 	}
-}
-// склонение числительных
-function declensionNum(num, words) {
-	return words[(num % 100 > 4 && num % 100 < 20) ? 2 : [2, 0, 1, 1, 1, 2][(num % 10 < 5) ? num % 10 : 5]];
-}
+});
+const transformStream2 = new Transform({
+	transform(chunk, encoding, callback) {
+		const transformedChunk = chunk.toString();
 
-function countdownTimer() {
-	let deadline = new Date(`${month}.${day}.${year} ${hour}:00 `);
-	let dataDeadline = `${deadline.getDate()}.${deadline.getMonth()+1}.${deadline.getFullYear()} ${deadline.getHours()}:00`;
+		let reg = new RegExp(`^.*(34.48.240.111).*$`, 'gm');
+		let result = transformedChunk.match(reg);
 
-	const now = new Date();
-	const timeDiff = Math.abs(deadline.getTime() - now.getTime());
-	const diff = new Date(timeDiff);
-
-	if (diff <= 0) {
-		clearInterval(timerId);
+		result.forEach(element => {
+			this.push(element + "\n");
+		});
+		callback();
 	}
-
-	const diffYears = timeDiff > 0 ? (diff.getUTCFullYear() - 1970) : 0;
-	const diffMonth = timeDiff > 0 ? diff.getUTCMonth() : 0;
-	const diffDays = timeDiff > 0 ? (diff.getUTCDate() - 1) : 0;
-	const diffHours = timeDiff > 0 ? diff.getUTCHours() : 0;
-	const diffMinutes = timeDiff > 0 ? diff.getUTCMinutes() : 0;
-	const diffSeconds = timeDiff > 0 ? diff.getUTCSeconds() : 0;
-
-	// склонение числительных
-	const diffYearsDeclensionNum = declensionNum(diffYears, ['год', 'года', 'лет']);
-	const diffMonthDeclensionNum = declensionNum(diffMonth, ['месяц', 'месяца', 'месяцев']);
-	const diffDaysDeclensionNum = declensionNum(diffDays, ['день', 'дня', 'дней']);
-	const diffHoursDeclensionNum = declensionNum(diffHours, ['час', 'часа', 'часов']);
-	const diffMinutesDeclensionNum = declensionNum(diffMinutes, ['минута', 'минуты', 'минут']);
-	const diffSecondsDeclensionNum = declensionNum(diffSeconds, ['секунда', 'секунды', 'секунд']);
-
-	//добавляем 0 в начало к значению , если число меньше 10 и само число не равно 0
-	const diffMonthAddZero = (diffMonth < 10 && diffMonth !== 0) ? '0' + diffMonth : diffMonth;
-	const diffDaysAddZero = (diffDays < 10 && diffDays !== 0) ? '0' + diffDays : diffDays;
-	const diffHoursAddZero = (diffHours < 10 && diffHours !== 0) ? '0' + diffHours : diffHours;
-	const diffMinutesAddZero = (diffMinutes < 10 && diffMinutes !== 0) ? '0' + diffMinutes : diffMinutes;
-	const diffSecondsAddZero = (diffSeconds < 10 && diffSeconds !== 0) ? '0' + diffSeconds : diffSeconds;
-
-	//проверяем значения на характер финиша, те === 0
-	const diffYearsRes = diffYears === 0 ? `${colors.bgMagenta('Таймер в годах завершен!')}` : `${colors.bgGreen(diffYears)} ${diffYearsDeclensionNum}`;
-	const diffMonthRes = (diffMonthAddZero === 0 && diffYears === 0) ? `${colors.bgMagenta('Таймер в месяцах завершен!')}` : `${colors.bgGreen(diffMonthAddZero)} ${diffMonthDeclensionNum}`;
-	const diffDaysRes = (diffDaysAddZero === 0 && (diffYears && diffMonthRes) === 0) ? `${colors.bgMagenta('Таймер в днях завершен!')}` : `${colors.bgGreen(diffDaysAddZero)} ${diffDaysDeclensionNum}`;
-	const diffHoursRes = (diffHoursAddZero === 0 && (diffYears && diffMonthRes && diffDaysRes) === 0) ? `${colors.bgMagenta('Таймер в часах завершен!')}` : `${colors.bgGreen(diffHoursAddZero)} ${diffHoursDeclensionNum}`;
-	const diffMinutesRes = (diffMinutesAddZero === 0 && (diffYears && diffMonthRes && diffDaysRes && diffHoursRes) === 0) ? `${colors.bgMagenta('Таймер в минутах завершен!')}` : `${colors.bgGreen(diffMinutesAddZero)} ${diffMinutesDeclensionNum}`;
-	const diffSecondsRes = (diffSecondsAddZero === 0 && (diffYears && diffMonthRes && diffDaysRes && diffHoursRes && diffMinutesRes) === 0) ? `${colors.bgMagenta('Таймер в секундах завершен!')}` : `${colors.bgGreen(diffSecondsAddZero)} ${diffSecondsDeclensionNum}`;
-
-	let result = `До ${colors.blue(dataDeadline)} осталось: 
-	 ${diffYearsRes} ${diffMonthRes} ${diffDaysRes} 
-	 ${diffHoursRes} : ${diffMinutesRes} : ${diffSecondsRes}
-	`;
-	console.clear();
-	console.log(result);
-}
-
-const timer = () => {
-	timerId = setInterval(countdownTimer, 1000);
-}
-const run = (hour, day, month, year) => {
-	let deadline = new Date(`${month}.${day}.${year} ${hour}:00 `);
-	checkingInputParameters(hour, day, month, year, deadline);
-};
+});
+readStream.pipe(transformStream1).pipe(writeStream1);
+readStream.pipe(transformStream2).pipe(writeStream2);
 
 
-emitter.on('timer', () => timer());
-emitter.on('errorNaN', () => errorNaN());
-emitter.on('errorLessCurrentDate', () => errorLessCurrentDate());
-emitter.on('finish', () => finish());
+///////ОПТИМАЛЬНОЕ РЕШЕНИЕ
 
-run(Number(hour), Number(day), Number(month), Number(year));
+// const fs = require('fs')
+// const readline = require('readline')
+
+// const readStream = fs.createReadStream('./access.log', 'utf8')
+// const writeStream1 = fs.createWriteStream('./89.123.1.41_requests.log')
+// const writeStream2 = fs.createWriteStream('./34.48.240.111_requests.log')
+
+// let numStr = 0
+
+// const rl = readline.createInterface({
+// 	input: readStream,
+// 	terminal: true
+// });
+
+// rl.on('line', (line) => {
+// 	// сделать получение ip из строки и проверку через includes
+// 	if (line.includes("89.123.1.41")) {
+// 		writeStream1.write(line + "\n")
+// 	}
+
+// 	if (line.includes("34.48.240.111")) {
+// 		writeStream2.write(line + "\n")
+// 	}
+
+// 	console.log(++numStr)
+// })
